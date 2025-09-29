e60c9514b8536310e561cb59116ff2d1fd00fbaf
nf4r4r4n <baibs.fararano@gmail.com>
2025-09-29 15:16:28 +0300

    update: adding tailwindcss and markdown extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -31,6 +31,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.editor.telescope" },
     -- import/override with your plugins
     { import = "plugins" },
